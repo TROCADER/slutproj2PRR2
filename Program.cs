6ab6816a065d14d6f04adc4f650a06d43a6b4876
William Mårback <william.marback@hotmail.com>
2021-03-31 11:11:33 +0200

    Check playerinput, dictionay, almost working shop

--- Program.cs
+++ Program.cs
@@ -1,37 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace slutproj2PRR2
 {
     class Program
     {
         public static Random random = new Random();
-
+        static Queue<string> playerNames = new Queue<string>();
+        static HashSet<string> playerInputs = new HashSet<string>();
+        static Dictionary<string, int> itemDict = new Dictionary<string, int>();
 
         static void Main(string[] args)
         {
             Book[] books = new Book[10];
-            Queue<string> playerNames = new Queue<string>();
-
-            System.Console.WriteLine("What should book be about?");
-
-            string playerInput = Console.ReadLine().Trim();
-            playerNames.Enqueue(playerInput);
+            string playerInput;
+            int playerMoney = 1000;
 
             for (int i = 0; i < books.Length; i++)
             {
+                System.Console.WriteLine("What should be the name of the book");
                 playerInput = Console.ReadLine().Trim();
+
+                while (playerInput == "" || playerInputs.Contains(playerInput))
+                {
+                    System.Console.WriteLine("Name is either already used, or not valid, please try again");
+                    playerInput = Console.ReadLine().Trim();
+                }
+
+                playerInputs.Add(playerInput);
                 playerNames.Enqueue(playerInput);
             }
-            
+
             for (int i = 0; i < books.Length; i++)
             {
-                books[i] = new Book(playerInput);
+                books[i] = new Book(playerNames.Dequeue());
+
+                itemDict.Add(books[i].Name, books[i].Cost);
             }
 
+            System.Console.WriteLine("This are the items we sell:");
+            foreach (string key in itemDict.Keys)
+            {
+                System.Console.WriteLine("Name: " + key + " | Price: " + itemDict[key]);
+            }
 
-            System.Console.WriteLine("Welcome to shop!");
-            System.Console.WriteLine("We sell this things: ");
+            System.Console.WriteLine("Which do you want to buy?\nType the indexnumber of item");
+            playerInput = Console.ReadLine().Trim();
+
+            int convertedString = StringToInt(playerInput);
+
+            playerMoney -= itemDict.ElementAt(convertedString).Value;
+            itemDict.Remove(itemDict.ElementAt(convertedString).Key);
+
+            foreach (string key in itemDict.Keys)
+            {
+                System.Console.WriteLine("Name: " + key + " | Price: " + itemDict[key]);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int StringToInt(string playerInput)
+        {
+            int result = 0;
+            bool sucess = int.TryParse(playerInput, out result);
+
+            while (sucess != true)
+            {
+                playerInput = Console.ReadLine().Trim();
+                sucess = int.TryParse(playerInput, out result);
+            }
+
+            return result;
         }
     }
 }
