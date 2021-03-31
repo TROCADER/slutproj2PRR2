47dc0d175036113311f3d6d35ebf9f20eacd3d62
William Mårback <william.marback@hotmail.com>
2021-03-31 11:12:16 +0200

    Bookclass somehow did not push to git

--- Book.cs
+++ Book.cs
@@ -5,12 +5,12 @@ namespace slutproj2PRR2
     public class Book
     {
         public int Cost {get; set;}
-        public string Info {get; set;}
+        public string Name {get; set;}
 
         public Book(string playerInput)
         {
             Cost = Program.random.Next(1, 101);
-            Info = playerInput;
+            Name = playerInput;
         }
     }
 }
